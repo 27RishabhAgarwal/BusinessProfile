7297d4274fa213c96a146afd4f8ec3ada522a19e
themanojrawat <mmkumarrawat8055@gmail.com>
2024-03-04 19:28:07 +0530

    app store added

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,9 +1,11 @@
 const VideoBackground = () => {
     return (
-        <div className="video-background" style={{ backgroundImage: `url(./assets/background.jpg)` }} >
+        <div className="video-background" style={{ backgroundImage: `url(https://cdn0070.qrcodechimp.com/images/digitalCard/bg/background_15.jpg?v=1708081427c)` }} >
         {/* <video autoPlay muted loop playsInline>
             <source src="./assets/background.mp4" />
-        </video> */}
+        </video> 
+        "homepage": "https://27RishabhAgarwal.github.io/BusinessProfile",
+        */}
        
 
         </div>
--- src/components/CardButtons.jsx
+++ src/components/CardButtons.jsx
@@ -1,16 +1,47 @@
-const CardButtonBox = () => {
-    return(
-    <div className="card-button-box">
-        <div className="card-button-box-title">
-            <h2>Partner Apps</h2>
-            <p>Install to get extra benifits</p>
-        </div>
-        <div className="card-button-box-items">
-            <a href="https://play.google.com/store/apps/details?id=com.drivetrackplusrefuel" className="button" target="_blank" rel="noreferrer">HP Pay</a>
-            <a href="https://play.google.com/store/apps/details?id=com.drivetrackplus" className="button" target="_blank" rel="noreferrer">Drive Track Plus</a>
-        </div>
-    </div>
-    );
-};
+import React from 'react';
 
-export default CardButtonBox;
+    const CardButtonBox = () => {
+        const handleButtonClick = (link) => {
+            if (navigator.userAgent.match(/Android/i)) {
+                window.open(link.android, '_blank');
+            } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+                window.open(link.ios, '_blank');
+            }
+        };
+
+        const apps = [
+            {
+                name: 'HP Pay',
+                android: 'https://play.google.com/store/apps/details?id=com.drivetrackplusrefuel',
+                ios: 'https://apps.apple.com/in/app/hp-pay/id1343241227',
+            },
+            {
+                name: 'Drive Track Plus',
+                android: 'https://play.google.com/store/apps/details?id=com.drivetrackplus',
+                ios: 'https://apps.apple.com/in/app/drivetrack-plus-customer-app/id1061140479',
+            },
+        ];
+
+        return (
+            <div className="card-button-box">
+                <div className="card-button-box-title">
+                    <h2>Partner Apps</h2>
+                    <p>Install to get extra benefits</p>
+                </div>
+                <div className="card-button-box-items">
+                    {apps.map((app, index) => (
+                        <a
+                            key={index}
+                            href="# "
+                            className="button"
+                            onClick={() => handleButtonClick(app)}
+                        >
+                            {app.name}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
+    export default CardButtonBox;
